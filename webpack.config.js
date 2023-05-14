67eb720d140497cf44a1212b9f90cfd48e3dba8b
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-05-14 14:44:33 +0530

    Added Mini Css Extract Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -8,11 +9,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.js$/,
@@ -26,21 +37,12 @@ module.exports = {
       },
     ],
   },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'webpack App',
+      title: 'Webpack App',
       filename: 'index.html',
       template: './src/index.html',
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
